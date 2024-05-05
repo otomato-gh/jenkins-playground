ffd3c2facd2209d0b11e638c6565ff48a94f8021
Ant Weiss <anton@otomato.io>
2024-05-05 17:00:19 +0300

    Update setup_jenkins.sh

--- non-docker/setup_jenkins.sh
+++ non-docker/setup_jenkins.sh
@@ -1,3 +1,7 @@
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update && sudo apt install -y default-jre && sudo apt install -y jenkins
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
